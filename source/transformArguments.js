7d099022d3fdf29aae16dbeb7aec8bc4b1b5b7f9
Marthinus Engelbrecht <mj.engelbrecht@gmail.com>
2016-04-16 07:38:41 +0200

    Refactored transform arguments function and allow latedef

--- source/transformArguments.js
+++ source/transformArguments.js
@@ -1,6 +1,19 @@
 import parseArgs from 'minimist';
 import * as ErrorTypes from './constants/errorTypes';
 
+function transformArguments(args) {
+    const normalizedArgs = args.slice(2);
+    if (normalizedArgs[ 0 ] === 'run') {
+        return transformValidArguments(normalizedArgs);
+    }
+    else {
+        throw {
+            name: ErrorTypes.INVALID_COMMAND,
+            message: `'${normalizedArgs[ 0 ]}' is not a valid command`
+        };
+    }
+}
+
 function objectToKeyValuePairArray(obj) {
     let array = [];
     for (var key in obj) {
@@ -11,20 +24,21 @@ function objectToKeyValuePairArray(obj) {
     }
 }
 
-export default function transformArguments(args) {
-    const normalizedArgs = args.slice(2);
-    if (normalizedArgs[ 0 ] === 'run') {
-        let parsedArgs = parseArgs(normalizedArgs.slice(1));
-        const path = parsedArgs._[ 0 ];
-        delete parsedArgs._;
-        const keyValuePairArray = objectToKeyValuePairArray(parsedArgs);
-        const options = new Map(keyValuePairArray);
-        return [ path, options ];
-    }
-    else {
-        throw {
-            name: ErrorTypes.INVALID_COMMAND,
-            message: `'${normalizedArgs[ 0 ]}' is not a valid command`
-        };
-    }
+function extractPath(parsedArgs) {
+    const path = parsedArgs._[ 0 ];
+    delete parsedArgs._;
+    return path;
 }
+
+function extractOptions(parsedArgs) {
+    return new Map(objectToKeyValuePairArray(parsedArgs));
+}
+
+function transformValidArguments(normalizedArgs) {
+    let parsedArgs = parseArgs(normalizedArgs.slice(1));
+    const path = extractPath(parsedArgs);
+    const options = extractOptions(parsedArgs);
+    return [ path, options ];
+}
+
+export default transformArguments;
